c5f6950b49cd60d552f808bfb9b7feaf2be867c1
Richard Anthony <public.rant@pm.me>
2022-04-09 21:31:32 +1000

    Lecture Video Collection only collects videos

--- lib/models/lecture_video_collection.rb
+++ lib/models/lecture_video_collection.rb
@@ -1,106 +1,19 @@
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'mechanize'
-require 'net/http'
-require 'open-uri'
-require 'json'
 
 # nodoc
 class LectureVideoCollection
-  attr_reader :collection_name
-  def initialize(attributes = {}, collection_name)
+  def initialize(attributes = {})
     @attributes = attributes
-    @buffer = {}
-    @collection_name = collection_name
   end
 
-  def enqueue_for_download
-    video_courses.flat_map do |course|
-      puts '#' * 50, course['href']
-      response = Fetcher.new(course).response
-
-      @buffer[course['href']] = [Document.new(response).titles]
-    end
-
-    File.open("/work/tmp/#{collection_name}.json", 'w') do |file|
-    #   then run  a container with the environment
-    #   and babysit until they are all downloaded
-    #   maybe even starting with lowest quality audio
-    #   then video
-    #   and thumbnails
-    # individuaal environment files for each container
-    #   notes/course['href']
-    #     export COURSE_HREF=
-    #     export LECTURE_TITLE=
-      file.write @buffer.to_json
-    end
-  end
-
-  def video_courses
-    @video_courses ||= @attributes.select do |course|
+  def to_a
+    @video_collection ||= attributes.select do |course|
       course['completeVideo'] == true
     end
   end
-end
 
-# nodoc
-class Fetcher
-  attr_reader :course
+  private
 
-  def initialize(course)
-    @course = course
-    @buffer = []
-  end
-
-  PATTERNS = %w[lecture-videos video-lectures]
-  DOMAIN = 'https://ocw.mit.edu'
-
-  def bot
-    return @bot if @bot
-
-    @bot ||= Mechanize.new
-    @bot.follow_meta_refresh
-    @bot.redirect_ok
-    @bot
-  end
-
-  EmptyTempFile = Class.new Tempfile
-
-  def response
-    PATTERNS.each do |pattern|
-      @buffer << bot.get(URI("#{url}/#{pattern}"))
-    rescue Mechanize::ResponseCodeError
-      @buffer << EmptyTempFile.new
-    end
-
-    @buffer.select do |obj|
-      obj.is_a? Mechanize::Page
-    end.first || Nokogiri::HTML(Tempfile.new)
-  end
-
-  def url
-    "#{DOMAIN}/#{course['href']}"
-  end
-end
-
-# nodoc
-class Document
-  attr_reader :response
-
-  def initialize(response)
-    @response = response
-  end
-
-  def titles
-    @titles ||= document.css('.medialisting a').map do |link|
-      link.attr('title')
-    end
-
-    @titles.compact
-  end
-
-  def document
-    response
-  end
+  attr_reader :attributes
 end
--- spec/models/lecture_video_collection_spec.rb
+++ spec/models/lecture_video_collection_spec.rb
@@ -1,36 +1,21 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require 'pry'
+require 'json'
 require_relative '../../lib/models/lecture_video_collection'
 
 RSpec.describe LectureVideoCollection do
-  let(:fixed_data) do
+  let(:fixture) do
     File.read 'spec/fixture.json'
   end
 
-  let(:fixed_attributes) do
-    JSON.parse fixed_data
-  end
   subject(:lecture_video_collection) do
-    described_class.new fixed_attributes, 'engineering'
-  end
-  describe '#collect' do
-    context 'when the response is a 404' do
-      it 'matches againsts the alternate pattern'
-      it 'logs the attempted request'
-    end
-    it 'enumerates the videos in the collection' do
-      expect(lecture_video_collection.video_courses.count).to eql 85
-    end
+    described_class.new JSON.parse(fixture)
   end
 
-  describe '#enqueue_for_download' do
-    subject(:queue) do
-      lecture_video_collection.enqueue_for_download
-    end
-    it 'sends a batch to the downloader' do
-      expect(subject.count).to eql 14
+  describe '#collect' do
+    it 'enumerates the videos in the collection' do
+      expect(lecture_video_collection.to_a.count).to eql 85
     end
   end
 end
