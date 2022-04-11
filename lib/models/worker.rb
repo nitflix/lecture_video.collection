c3c70896c759961ebf5b2956d0d033a9093bdd95
Richard Anthony <public.rant@pm.me>
2022-04-11 18:56:04 +1000

    fixup! Worker

--- lib/models/worker.rb
+++ lib/models/worker.rb
@@ -1,19 +1,29 @@
 require 'redis'
+require_relative './client'
 
 # @nodoc
 class Worker
-  def initialize(db, collection)
+  def initialize(db, collection, client)
     @db = db
     @collection = collection
+    @client = client
   end
 
   def write
     collection.each do |entry|
-      db.set 'foo', entry
+      db.set entry[:courseHref], []
+    end
+  end
+
+  def work
+    collection.each do |entry|
+      href = entry[:courseHref]
+      document_titles = client.update(href).document_titles
+      db.set href, document_titles
     end
   end
 
   private
 
-  attr_reader :db, :collection
+  attr_reader :db, :collection, :client
 end
--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -10,29 +10,45 @@ RSpec.describe Worker do
   end
 
   describe '#write' do
-
+    after do
+      # db.del('stubbed')
+    end
     subject(:worker) do
-      described_class.new db, collection, client
+      described_class.new db, mock_collection, client
     end
     let(:collection) do
       double(LectureVideoCollection)
     end
-
     let(:mock_collection) do
       14.times.map do
-        {}
+        { courseHref: 'stubbed' }
       end
     end
-    it 'adds an entry to the database for each video in the collection' do
-      expect(collection).to receive(:each).and_return mock_collection
-      expect(db).to receive(:set).exactly(14).times
+    xit 'adds an entry to the database for each video in the collection' do
+      # expect(collection).to receive(:each).and_return mock_collection
+      expect(db).to receive(:set).with('stubbed', []).exactly(14).times
       worker.write
     end
 
-    it 'writes the response from the client to the entry in the database' do
-      expect(collection).to receive(:each).and_return mock_collection
-      expect(db).to receive(:set).exactly(14).times
-      worker.work
+    describe 'writing document titles to the db' do
+      let(:document_titles) do
+        ['test']
+      end
+      let(:courseHref) do
+        'courses/6-006-introduction-to-algorithms-spring-2020'
+      end
+      let(:example_collection) do
+        [{ courseHref: courseHref }]
+      end
+      subject do
+        described_class.new db, example_collection, client
+      end
+      it 'writes the response from the client to the entry in the database' do
+        allow(client).to receive(:update).and_return client
+        expect(client).to receive(:document_titles).and_return document_titles
+        expect(db).to receive(:set).with(courseHref, document_titles).exactly(1).times
+        subject.work
+      end
     end
   end
 end
